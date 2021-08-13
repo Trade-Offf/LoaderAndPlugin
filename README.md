789f9ce22a34931dac35973b7b2c35cb47091209
jia_lin.li <jia_lin.li@ceasia.cn>
2021-08-13 17:10:41 +0800

    install and config clean-webpack-pliugin

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 开发一个可以加载 markdown 文件的加载器，以便可以在代码中直接导入 md 文件;
 
 markdown 一般是需要转换为 html 之后再呈现到页面上的，所以我希望导入 md 文件后，直接得到 markdown 转换后的 html 字符串;
-
-markdown Loader用到了marked模块，需要npm安装。用来将Markdown解析为HTML模块
-$ npm i marked
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,24 @@
 // ./webpack.config.js
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./src/main.js",
 
   output: {
     filename: "bundle.js",
+    path: path.join(__dirname, "dist"),
   },
+
+  plugins: [new CleanWebpackPlugin()],
 
   module: {
     rules: [
       {
         test: /\.md$/,
-
         // 直接使用相对路径
-
-        use: ["html-loader", "./markdown-loader2"],
+        use: "./markdown-loader",
+        // use: ["html-loader", "./markdown-loader"],
       },
     ],
   },
