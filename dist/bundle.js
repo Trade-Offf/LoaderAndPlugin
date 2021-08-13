280e793660bc8f28e755f64e5d0c95c1e01a5d7b
jia_lin.li <jia_lin.li@ceasia.cn>
2021-08-13 17:58:45 +0800

    completed Plugin

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,1 +1,98 @@
-(()=>{var r={714:r=>{r.exports='\x3c!-- ./src/about.md --\x3e\n\n<h1 id="about">About</h1>\n<p>this is a markdown file.</p>\n'}},e={};function o(t){var n=e[t];if(void 0!==n)return n.exports;var a=e[t]={exports:{}};return r[t](a,a.exports,o),a.exports}o.n=r=>{var e=r&&r.__esModule?()=>r.default:()=>r;return o.d(e,{a:e}),e},o.d=(r,e)=>{for(var t in e)o.o(e,t)&&!o.o(r,t)&&Object.defineProperty(r,t,{enumerable:!0,get:e[t]})},o.o=(r,e)=>Object.prototype.hasOwnProperty.call(r,e),(()=>{"use strict";var r=o(714),e=o.n(r);console.log(e())})()})();
+(() => {
+  // webpackBootstrap
+  var __webpack_modules__ = [
+    ,
+    /* 0 */ /* 1 */
+    (module) => {
+      module.exports =
+        '<!-- ./src/about.md -->\n\n<h1 id="about">About</h1>\n<p>this is a markdown file.</p>\n';
+    },
+  ];
+  // The module cache
+  var __webpack_module_cache__ = {};
+
+  // The require function
+  function __webpack_require__(moduleId) {
+    // Check if module is in cache
+    var cachedModule = __webpack_module_cache__[moduleId];
+    if (cachedModule !== undefined) {
+      return cachedModule.exports;
+    }
+    // Create a new module (and put it into the cache)
+    var module = (__webpack_module_cache__[moduleId] = {
+      // no module.id needed
+      // no module.loaded needed
+      exports: {},
+    });
+
+    // Execute the module function
+    __webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+
+    // Return the exports of the module
+    return module.exports;
+  }
+
+  /* webpack/runtime/compat get default export */
+  (() => {
+    // getDefaultExport function for compatibility with non-harmony modules
+    __webpack_require__.n = (module) => {
+      var getter =
+        module && module.__esModule ? () => module["default"] : () => module;
+      __webpack_require__.d(getter, { a: getter });
+      return getter;
+    };
+  })();
+
+  /* webpack/runtime/define property getters */
+  (() => {
+    // define getter functions for harmony exports
+    __webpack_require__.d = (exports, definition) => {
+      for (var key in definition) {
+        if (
+          __webpack_require__.o(definition, key) &&
+          !__webpack_require__.o(exports, key)
+        ) {
+          Object.defineProperty(exports, key, {
+            enumerable: true,
+            get: definition[key],
+          });
+        }
+      }
+    };
+  })();
+
+  /* webpack/runtime/hasOwnProperty shorthand */
+  (() => {
+    __webpack_require__.o = (obj, prop) =>
+      Object.prototype.hasOwnProperty.call(obj, prop);
+  })();
+
+  /* webpack/runtime/make namespace object */
+  (() => {
+    // define __esModule on exports
+    __webpack_require__.r = (exports) => {
+      if (typeof Symbol !== "undefined" && Symbol.toStringTag) {
+        Object.defineProperty(exports, Symbol.toStringTag, { value: "Module" });
+      }
+      Object.defineProperty(exports, "__esModule", { value: true });
+    };
+  })();
+
+  var __webpack_exports__ = {};
+  // This entry need to be wrapped in an IIFE because it need to be in strict mode.
+  (() => {
+    "use strict";
+    __webpack_require__.r(__webpack_exports__);
+    /* harmony import */ var _about_md__WEBPACK_IMPORTED_MODULE_0__ =
+      __webpack_require__(1);
+    /* harmony import */ var _about_md__WEBPACK_IMPORTED_MODULE_0___default =
+      /*#__PURE__*/ __webpack_require__.n(
+        _about_md__WEBPACK_IMPORTED_MODULE_0__
+      );
+    // ./src/main.js
+
+    console.log(_about_md__WEBPACK_IMPORTED_MODULE_0___default());
+
+    // 希望 about => '<h1>About</h1><p>this is a markdown file.</p>'
+  })();
+})();
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,19 @@
 // ./webpack.config.js
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const RemoveCommentsPlugin = require("./remove-comments-plugin");
 
 module.exports = {
   entry: "./src/main.js",
+
+  mode: "none", // 不使用任何默认优化选项，让打包文件更易阅读
 
   output: {
     filename: "bundle.js",
     path: path.join(__dirname, "dist"),
   },
 
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [new CleanWebpackPlugin(), new RemoveCommentsPlugin()],
 
   module: {
     rules: [
